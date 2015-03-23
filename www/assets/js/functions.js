bc07a86d9e9ac0283de747f28fdb85f1610424f3
Gilles Vandenoostende <gilles.vandenoostende@duvalguillaume.com>
2015-03-23 14:30:34 +0100

    basic UI, AJAX placeholder, canonical grid progress

--- www/assets/js/functions.js
+++ www/assets/js/functions.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 	
-	alert("lol");
+	// references
+	var $container = $('#container');
+	
+	// STEP 1: load the data
+	$container.load("/grid.php", function(evt){
+		//alert("loaded");
+	});
 	
 });
--- www/grid.php
+++ www/grid.php
@@ -6,5 +6,31 @@
 	
 	$numCols = 20;
 	$numRows = 30;
+	
+	for($i = 0; 
+		$i < $numRows;
+		$i ++):
+?>
 
+	<div class="row">
+		
+		<?php
+			
+			for($j = 0;
+				$j < $numCols;
+				$j ++):			
+		?>
+		
+			<div class="micromonial" id="micromonial-<?php echo($j + $i*$numCols); ?>" title="micromonial-<?php echo($j + $i*$numCols); ?>">
+				<img src="/generator/140x230" alt="" />
+			</div>
+		
+		<?php
+			endfor;
+		?>
+		
+	</div>
+
+<?php
+	endfor;
 ?>
