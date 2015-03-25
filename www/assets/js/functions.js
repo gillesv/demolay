f53de41dd746a2679c227f0c3e4dc42404778251
Gilles Vandenoostende <gilles.vandenoostende@duvalguillaume.com>
2015-03-25 13:59:40 +0100

    automatic centering cross-device

--- www/assets/js/functions.js
+++ www/assets/js/functions.js
@@ -3,32 +3,39 @@ $(document).ready(function() {
 	// references
 	var $main = $('#main'),
 		$container = $('#container'),
+		$window = $(window),
 		touch = Modernizr.touch;
 	
 	// STEP 1: load the data
 	$container.load("/grid.php", function(evt){
-		//alert("loaded");
+		// while we're waiting 500ms, show some animation or whatever
 		
-		if(!touch) {
-			initDesktopScrolling();
-		}
+		setTimeout(function() {
+			snapToCenter();
+			
+			if(!touch) {
+				initDesktopScrolling();
+			}
+		}, 500);
+	});
+	
+	// wait for the layout to be finished, then measure
+	function snapToCenter() {
+		var w1 = $container.width(),
+			h1 = $container.height(),
+			w2 = $window.width(),
+			h2 = $window.height();
 		
 		// overflow toss to center
-		/*
-		var h = $container.height();
-		
-		$main.css({height: h + 'px'});
-		*/
-		
 		overthrow.toss(
 			$main[0], 
 			{
-				top: "+600",
-				left: 500,
-				duration: 600
+				left: (w1 - w2)/2,
+				top: (h1 - h2)/2,
+				duration: 0	// instant, show animation
 			}
 		);
-	});
+	}
 	
 	// STEP 2: desktop drag scrolling
 	
@@ -38,6 +45,8 @@ $(document).ready(function() {
 	
 		
 	function initDesktopScrolling () {
+		
+	
 		/*
 		window.addEventListener("mousedown", function(e) {
 			curDown = true;
