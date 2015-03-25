cec9f9a703b1b03763461faecf3c4cb2a3784ff1
Gilles Vandenoostende <gilles.vandenoostende@duvalguillaume.com>
2015-03-25 15:42:08 +0100

    made a static version to deploy & test on github

--- www/grid.php
+++ www/grid.php
@@ -11,26 +11,20 @@
 		$i < $numRows;
 		$i ++):
 ?>
-
 	<div class="row">
-		
 		<?php
 			
 			for($j = 0;
 				$j < $numCols;
 				$j ++):			
 		?>
-		
 			<div class="micromonial" id="micromonial-<?php echo($j + $i*$numCols); ?>" title="micromonial-<?php echo($j + $i*$numCols); ?>">
-				<img src="/generator/172x286/ccc/fff&text=<?php echo($j + 1 + $i*$numCols); ?>" alt="" />
+				<img src="/assets/img/placeholder.png" alt="<?php echo($j + 1 + $i*$numCols); ?>" /> <?php /* <img src="/generator/172x286/ccc/fff&text=<?php echo($j + 1 + $i*$numCols); ?>" alt="" /> */ ?>
 			</div>
-		
 		<?php
 			endfor;
 		?>
-		
 	</div>
-
 <?php
 	endfor;
 ?>
