2f6bbe44e614b5d0dd00ebb24d7dbb54fbd7d68e
Gilles Vandenoostende <gilles.vandenoostende@duvalguillaume.com>
2015-03-25 15:11:44 +0100

    scrolling by clicking & dragging accross desktop & mobile

--- www/assets/js/functions.js
+++ www/assets/js/functions.js
@@ -12,10 +12,6 @@ $(document).ready(function() {
 		
 		setTimeout(function() {
 			snapToCenter();
-			
-			if(!touch) {
-				initDesktopScrolling();
-			}
 		}, 500);
 	});
 	
@@ -35,37 +31,76 @@ $(document).ready(function() {
 				duration: 0	// instant, show animation
 			}
 		);
+		
+		setTimeout(function(){
+			if(!touch) {
+				initDesktopScrolling();
+			};
+		}, 500);
 	}
 	
 	// STEP 2: desktop drag scrolling
 	
 	var curXPos = 0,
 		curYPos = 0,
+		curScrollLeft = 0,
+		curScrollTop = 0,
+		deltaX = 0,
+		deltaY = 0,
+		maxScrollLeft = 0,
+		maxScrollTop = 0,
 		curDown = false;
 	
 		
 	function initDesktopScrolling () {
-		
+		curScrollLeft = $main.scrollLeft();
+		curScrollTop = $main.scrollTop();
+		maxScrollLeft = $main[0].scrollWidth - $main.width();
+		maxScrollTop = $main[0].scrollHeight - $main.height();
 	
-		/*
 		window.addEventListener("mousedown", function(e) {
 			curDown = true;
 			curYPos = e.pageY;
 			curXPos = e.pageX;
+			curScrollLeft = $main.scrollLeft();
+			curScrollTop = $main.scrollTop();
 		});
-		
+	
 		window.addEventListener("mouseup", function(e) {
 			curDown = false;
+			
+			deltaX = deltaY = 0;
+		});
+	
+		window.addEventListener("mousemove", function(e) {
+			if(curDown) {
+				deltaX = curXPos - e.pageX;
+				deltaY = curYPos - e.pageY;
+			};
 		});
 		
-		window.addEventListener("mousemove", function(e) {
-			if(curDown === true) {
-				//console.log(curXPos + " " + e.pageX + " " + curYPos + " " + e.pageY);
-				
-				$main.scrollLeft(curXPos - e.pageX);
-				$main.scrollTop(curYPos + e.pageY);
+		function moveScroll() {
+			if(curDown) {			
+				$main.scrollLeft(curScrollLeft + deltaX);
+				$main.scrollTop(curScrollTop + deltaY);
 			}
-		});
-		*/
+		}
+		
+		(function animationLoop() {
+			requestAnimFrame(animationLoop);
+			moveScroll();
+		})();
 	}
 });
+
+// RAF fallback by http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
